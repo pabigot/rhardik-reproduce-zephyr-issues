8a20977cc444c6df5809b7aebee87e39dc0eb472
hardik.kishore <hardik.kishore@embdestech.co.in>
2020-04-08 18:32:31 +0530

    Unknown message received by the node

---
+++ README.md
@@ -0,0 +1,1 @@
+# reproduce-zephyr-issues
---
+++ node-a/src/user_cfg.h
@@ -0,0 +1,25 @@
+#ifndef USER_CFG_H
+#define USER_CFG_H
+
+static struct bt_mesh_cfg_srv cfg_srv = {
+	.relay = BT_MESH_RELAY_DISABLED,
+	.beacon = BT_MESH_BEACON_DISABLED,
+#if defined(CONFIG_BT_MESH_FRIEND)
+	.frnd = BT_MESH_FRIEND_ENABLED,
+#else
+	.frnd = BT_MESH_FRIEND_NOT_SUPPORTED,
+#endif
+#if defined(CONFIG_BT_MESH_GATT_PROXY)
+	.gatt_proxy = BT_MESH_GATT_PROXY_ENABLED,
+#else
+	.gatt_proxy = BT_MESH_GATT_PROXY_NOT_SUPPORTED,
+#endif
+	.default_ttl = 7,
+
+	/* 3 transmissions with 20ms interval */
+	.net_transmit = BT_MESH_TRANSMIT(2, 20),
+	.relay_retransmit = BT_MESH_TRANSMIT(2, 20),
+//	.hb_sub.func = heartbeat,
+};
+
+#endif /*USER_CFG_H*/
